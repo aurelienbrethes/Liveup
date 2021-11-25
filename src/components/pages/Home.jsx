45cf3f5b3617926b2deb36932ea44eaea224bda4
aurelienbrethes <aurelienbrethes@gmail.com>
2021-11-25 23:54:19 +0100

    carousel position ok

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -41,8 +41,7 @@ const Home = () => {
                         )})}
                     </select>
                 }
-            <Carousel/>
-            <EventCard />            
+                <Carousel/>        
             <div className="mapDiv">
                 <iframe src="https://www.google.com/maps/d/embed?mid=1PZ20cNpFYQgxdScopsIzGguV4Vo1oayL" width="640" height="480"></iframe>
             </div>
