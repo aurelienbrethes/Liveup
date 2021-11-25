f7de2c09fa7e3a3a42d6d9d8c7324e264be8c147
DavidVM19 <davidvm1910@gmail.com>
2021-11-25 23:21:41 +0100

    add filters & axios event

--- src/components/pages/Event.jsx
+++ src/components/pages/Event.jsx
@@ -1,15 +1,57 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import EventCard from '../EventCard';
 import './event.css';
+import axios from 'axios';
 
 const Event = () => {
+    const [event, setEvent] = useState([]);
+    const [style, setStyle] = useState([]);
+
+
+    useEffect(() => {
+        axios
+        .get('https://apiliveup.herokuapp.com/events')
+        .then((res) => res.data)
+        .then((data) => {
+            setEvent(data)
+        })
+    }, [])
+
+    useEffect(() => {
+        axios
+        .get('https://apiliveup.herokuapp.com/styles')
+        .then((res) => res.data)
+        .then((data) => {
+            setStyle(data)
+        })
+    }, [])
+
     return (
         <div className="event">
 
             <div className="eventFilters">
-                <input></input>
-                <input></input>
-                <input></input>
+                <select name="artistSelect" id="artistFilter">
+                    {event.map((info, index) => {
+                        return(
+                        <option key={index} value={info.artist_name}>{info.artist_name}</option>
+                    )})}
+                </select>
+                <select name="citySelect" id="cityFilter">
+                    {event.map((info) => { return ( info.city)})
+                    .reduce((unique, item) => unique.includes(item) ? unique : [...unique, item], [])
+                    .map((info, index) => {
+                        return (
+                        <option key={index} value={info}>{info}</option>
+                    )})}
+                </select>
+                <select name="styleSelect" id="styleFilter">
+                    {style.map((info) => { return ( info.name_style)})
+                    .reduce((unique, item) => unique.includes(item) ? unique : [...unique, item], [])
+                    .map((info, index) => {
+                        return (
+                        <option key={index} value={info}>{info}</option>
+                    )})}
+                </select>
             </div>
 
             <div className="eventList">
