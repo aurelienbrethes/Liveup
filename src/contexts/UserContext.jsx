785ed2f3e8cf1285e3505a26b95a8fb30d446dc0
Jimmy <jimmy.ganci@gmail.com>
2021-11-26 04:32:45 +0100

    added context

--- src/contexts/UserContext.jsx
+++ src/contexts/UserContext.jsx
@@ -1,21 +1,21 @@
-import { createContext, useState } from 'react';
-import axios from 'axios';
+import { createContext, useState } from "react";
+import axios from "axios";
 
 const UserContext = createContext(null);
 
 export default UserContext;
 
-export const UserContext.Provider = ({children}) => {
-	const [userConnected, setUserConnected] = useState([]);
-	axios
-	.get('https://apiliveup.herokuapp.com/login')
-	.then((res)=> res.data)
-	.then((data) => setUserConnected(data))
-	.catch((err) => console.log(err);)
+export const UserContextProvider = ({ children }) => {
+  const [userConnected, setUserConnected] = useState([]);
+  axios
+    .get("https://apiliveup.herokuapp.com/login")
+    .then((res) => res.data)
+    .then((data) => setUserConnected(data))
+    .catch((err) => console.log(err));
 
-	return (
-		<UserContext.Provider value={{userConnected, setUserConnected}}>
-		{children}
-		</UserContext.Provider>
-	);
+  return (
+    <UserContext.Provider value={{ userConnected, setUserConnected }}>
+      {children}
+    </UserContext.Provider>
+  );
 };
