90b7f83c237f8667c94e34fc2b451332a41125b0
aurelienbrethes <aurelienbrethes@gmail.com>
2021-11-26 10:07:29 +0100

    grande modale ok

--- src/components/pages/Event.jsx
+++ src/components/pages/Event.jsx
@@ -3,6 +3,23 @@ import EventCard from '../EventCard';
 import './event.css';
 import axios from 'axios';
 import Modal from '../Modal';
+import autres from '../medias/autres.png';
+import blues from '../medias/blues.jpg';
+import country from '../medias/country.jpg';
+import electro from '../medias/electro.jpg';
+import folk from '../medias/folk.jpg';
+import hip_hop from '../medias/hip_hop.png';
+import house from '../medias/house.jpg';
+import jazz from '../medias/jazz.png';
+import metal from '../medias/metal.jpg';
+import pop from '../medias/pop.jpg';
+import punk from '../medias/punk.png';
+import rap from '../medias/rap.jpg';
+import reggae from '../medias/reggae.jpg';
+import rnb from '../medias/rnb.jpg';
+import rock from '../medias/rock.jpg';
+import soul from '../medias/soul.jpg';
+import funk from '../medias/funk.jpg';
 
 const Event = () => {
     const [event, setEvent] = useState([]);
@@ -26,8 +43,9 @@ const Event = () => {
         .then((res) => res.data)
         .then((data) => {
             setStyle(data)
+            console.log(data)
         })
-    }, [])
+    }, []);
 
       /* Modal */
   const showModal = (id) => {
@@ -37,6 +55,65 @@ const Event = () => {
     setOpenModal('');
     console.log('hide');
   };
+
+  let image_style;
+
+  const selectImages = (element) => {
+
+    switch (element) {
+        case 5:
+            image_style = rock;
+        break;
+        case 15:
+            image_style = pop;
+        break;
+        case 25:
+            image_style = jazz;
+        break;
+        case 35:
+            image_style = soul;
+        break;
+        case 45:
+            image_style = rap;
+        break;
+        case 55:
+            image_style = folk;    
+        break;
+        case 65:
+            image_style = punk; 
+        break;
+        case 75:
+            image_style = metal; 
+        break;
+        case 85:
+            image_style = hip_hop;
+        break;
+        case 95:
+            image_style = rnb;
+        break;
+        case 105:
+            image_style = blues;
+        break;
+        case 115:
+            image_style = country;
+        break;
+        case 125:
+            image_style = funk;
+        break;
+        case 135:
+            image_style = reggae;
+        break;
+        case 145:
+            image_style = electro;
+        break;
+        case 155:
+            image_style = house;
+        break;
+        default:
+            image_style = autres;
+        break;
+    }
+}
 
     return (
         <div className="event">
@@ -78,9 +155,12 @@ const Event = () => {
                     return (
                 <div key={info.id}
                 aria-hidden="true">
-                    <div className="eventcard_container" onClick={() => showModal(info.id)}>
+                    <div className="eventcard_container" onClick={() =>
+                        showModal(info.id)
+                    }>
                     <EventCard />
                     </div>
+                    {selectImages(info.id)}
                     {info.id === openModal && (
                         <Modal
                           openModal={openModal}
@@ -88,7 +168,7 @@ const Event = () => {
                         >
                             <div className='modalGrid'>
                                 <aside className="left">
-                                    <img src="" alt="" />
+                                    <img width="400px" className="imageModale" src={image_style} alt="toto" />
                                 </aside>
                                 <aside className="right">
                                     <div className="modalHeader">
