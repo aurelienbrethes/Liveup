8312858218ba2bda11a52a1bfd24e4e86a26e424
Jimmy <jimmy.ganci@gmail.com>
2021-11-26 04:53:34 +0100

    change name css addevent.css

--- src/components/EventCard.jsx
+++ src/components/EventCard.jsx
@@ -1,15 +1,14 @@
-import React from 'react';
-import './eventCard.css';
-import eventImg from './medias/music-event-background.jpg';
+import React from "react";
+import "./eventCard.css";
+import eventImg from "./medias/music-event-background.jpg";
 
 const EventCard = () => {
-    
-    return (
-        <div className="eventCard">
-            <img src={eventImg} alt="Live event" />
-            <div className="eventTitle">Voir l'évènement</div>
-        </div>
-    );
+  return (
+    <div className="eventCard">
+      <img src={eventImg} alt="Live event" />
+      <div className="eventTitle">Voir l'évènement</div>
+    </div>
+  );
 };
 
 export default EventCard;
